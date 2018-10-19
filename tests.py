af991ad56149f30e1ad74694c5acd4d9c102db4b
radu <radu.fugaru@immfly.com>
2018-10-19 10:59:44 +0300

    tests comments

--- tests.py
+++ tests.py
@@ -3,6 +3,9 @@ from main import app
 
 
 class Tests(test.TestCase):
+    """
+    Testele nu sunt suficiente si nu sunt SOLID, pentru ca depind de gateway-uri. Ar fi trebuit sa fac un mock al acestora pentru testare.
+    """
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
